64d256bc514d01efb5e60e1fc8fe8b8a37a6b378
Selcuk Aksoy <Ooocrazyselcuk@gmail.com>
2021-05-07 17:02:12 +0300

    1.Stage
    1.stage completed without input controller. On the next stage I will add bouncer trap, electric trap and touch controller or maybe a gyro.

---
+++ Assets/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(this.gameObject.name == "Spiketrap")
+        Destroy(other.gameObject);
+        else
+        {
+            other.attachedRigidbody.AddRelativeForce(Vector3.back * 2000f);
+            //other.attachedRigidbody.AddExplosionForce(100f,other.gameObject.transform.position,30f);
+
+        }
+    }
+}
